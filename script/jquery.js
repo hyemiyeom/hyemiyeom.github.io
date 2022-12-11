168dbb12b4f3daf4ed34bd292d84a215f055b065
hyemiyeom <2023hdmi@gmail.com>
2022-12-11 14:56:16 +0900

    final add commit

--- script/jquery.js
+++ script/jquery.js
@@ -2,8 +2,14 @@ $(document).ready(function () {
 
   var list = 0;
   var list2 = 0;
+  var list3 = 0;
+  var list4 = 0;
+
   var count = $(".panel1 li").length;
   var count2 = $(".panel2 li").length;
+  var count3 = $(".panel3 li").length;
+  var count4 = $(".panel4 li").length;
+
   var autoSlide = setInterval(function(){
     if(list ==count-1){
       list = 0;
@@ -14,8 +20,7 @@ $(document).ready(function () {
     $(".panel1>li").eq(list).stop().fadeIn();
   }, 3000);
 
-
-  var autoSlide = setInterval(function(){
+  var autoSlide1 = setInterval(function(){
     if(list2 ==count2-1){
       list2 = 0;
     }else{
@@ -25,6 +30,25 @@ $(document).ready(function () {
     $(".panel2>li").eq(list).stop().fadeIn();
   }, 3000);
 
+  var autoSlide2 = setInterval(function(){
+    if(list3 ==count3-1){
+      list3 = 0;
+    }else{
+      list3++;
+    }
+    $(".panel3>li").stop().fadeOut();
+    $(".panel3>li").eq(list3).stop().fadeIn();
+  }, 3000);
+
+  var autoSlide3 = setInterval(function(){
+    if(list4 ==count4-1){
+      list4 = 0;
+    }else{
+      list4++;
+    }
+    $(".panel4>li").stop().fadeOut();
+    $(".panel4>li").eq(list4).stop().fadeIn();
+  }, 3000);
 
   $(".li.bars").click(function(){
     $("li.bars ul").css("display:block");
